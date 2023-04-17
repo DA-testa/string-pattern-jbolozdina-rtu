34f4f2f0185f7d1df184e5866033e480958dfddc
Jegors Bolozdiņa <125827886+jbolozdina-rtu@users.noreply.github.com>
2023-04-17 13:20:48 +0000

    solved

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,47 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    choice = input().strip()
+    if choice == 'F':
+        with open('test_sample.txt', 'r') as file:
+            pattern, text = file.readlines()
+    else:
+        pattern = input().strip()
+        text = input().strip()
+
+    return (pattern.rstrip(), text.rstrip())
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
+def rabin_karp(pattern, text):
+    p = 1000000007 # large prime number
+    x = 263 # random number
+    result = []
+
+    p_hash = 0
+    t_hash = 0
+    h = 1
+
+    for _ in range(len(pattern) - 1):
+        h = (h * x) % p
+
+    for i in range(len(pattern)):
+        p_hash = (x * p_hash + ord(pattern[i])) % p
+        t_hash = (x * t_hash + ord(text[i])) % p
+
+    for i in range(len(text) - len(pattern) + 1):
+        if p_hash == t_hash and text[i:i+len(pattern)] == pattern:
+            result.append(i)
+        if i < len(text) - len(pattern):
+            t_hash = (t_hash - ord(text[i]) * h) % p
+            t_hash = (t_hash * x + ord(text[i + len(pattern)])) % p
+            t_hash = (t_hash + p) % p
+
+    return result
+
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    return rabin_karp(pattern, text)
 
-    # and return an iterable variable
-    return [0]
-
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
